42e05ab37b0ca1a4ec7d7b5e732517ee34cf2224
FarhaN400 <fa07204377@gmail.com>
2025-08-27 20:18:17 +0530

    Update code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:exercise_app/constants.dart';
+import 'package:exercise_app/widget/category_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -15,74 +16,95 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         fontFamily: "Cairo",
         scaffoldBackgroundColor: kBackgroundColor,
-        textTheme: Theme.of(context).textTheme.apply(displayColor: kBackgroundColor),
+        textTheme: Theme.of(
+          context,
+        ).textTheme.apply(displayColor: kBackgroundColor),
       ),
       home: HomeScreen(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget{
-
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
-      return Scaffold(
-        body: Stack(
-          children: [
-            Container(
-              height: size.height * .45,
-              decoration: BoxDecoration(
-                color: Color(0xFFF5CEB8),
-                image: DecorationImage(image: AssetImage("assets/images/undraw_pilates_gpdb.png"),
-                    alignment: AlignmentGeometry.centerLeft),
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: size.height * .45,
+            decoration: BoxDecoration(
+              color: Color(0xFFF5CEB8),
+              image: DecorationImage(
+                image: AssetImage("assets/images/undraw_pilates_gpdb.png"),
+                alignment: AlignmentGeometry.centerLeft,
               ),
             ),
-            SafeArea(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Align(
-                      alignment: Alignment.topRight,
-                      child: Container(
-                        alignment: Alignment.center,
-                        width: 52,
-                        height: 52,
-                       decoration: BoxDecoration(
-                         color: Color(0xFFF2BEA1),
-                         shape: BoxShape.circle,
-                       ),
-                        child: SvgPicture.asset("assets/icons/menu.svg"),
-                      ),
-                    ),
-                    Text(
-                      "Good Morning \nFarhan",
-                      style: Theme.of(context).textTheme.displaySmall!.copyWith(fontWeight: FontWeight.w900,color: Colors.black),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.symmetric(vertical: 30),
-                      padding: const EdgeInsets.symmetric(horizontal: 30,vertical: 5),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      alignment: Alignment.center,
+                      width: 52,
+                      height: 52,
                       decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(49.5)
+                        color: Color(0xFFF2BEA1),
+                        shape: BoxShape.circle,
                       ),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: "Search",
-                          icon: SvgPicture.asset("assets/icons/search.svg"),
-                          border: InputBorder.none,
-                        ),
+                      child: SvgPicture.asset("assets/icons/menu.svg"),
+                    ),
+                  ),
+                  Text(
+                    "Good Morning \nFarhan",
+                    style: Theme.of(context).textTheme.displaySmall!.copyWith(
+                      fontWeight: FontWeight.w900,
+                      color: Colors.black,
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(vertical: 30),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 30,
+                      vertical: 5,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(49.5),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Search",
+                        icon: SvgPicture.asset("assets/icons/search.svg"),
+                        border: InputBorder.none,
                       ),
-                    )
-                  ],
-                ),
+                    ),
+                  ),
+                  Expanded(
+                    child: GridView.count(
+                      crossAxisCount: 2,
+                      childAspectRatio: .85,
+                      crossAxisSpacing: 20,
+                      mainAxisSpacing: 20,
+                      children: [
+                        CategoryCard(
+
+                        )
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
-        )
-      );
+          ),
+        ],
+      ),
+    );
   }
-  
 }
